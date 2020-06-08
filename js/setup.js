667f28109df4f37857f92b3242a0cd7d0707d190
Evgeniy <karatikus22@gmail.com>
2020-06-08 15:59:19 +0300

    добавление персонажей, генерация имени, цвета мантии и глаз

--- js/setup.js
+++ js/setup.js
@@ -10,40 +10,40 @@ var SETUP_WINDOW = document.querySelector('.setup');
 // активация окна установок
 SETUP_WINDOW.classList.remove('hidden');
 
-//показываем меню выбора персонажей
-document.querySelector('.setup-similar').classList.remove('hidden')
+// показываем меню выбора персонажей
+document.querySelector('.setup-similar').classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-//генерация имени и фаимилии персонажа
+// генерация имени и фаимилии персонажа
 var names = [];
-for (var j = 0; j < NAMES.length; j++){
+for (var j = 0; j < NAMES.length; j++) {
   var randomIndexName = Math.floor(Math.random() * NAMES.length);
   var randomIndexSurname = Math.floor(Math.random() * SURNAMES.length);
 
   var nameOut = NAMES[randomIndexName];
   var surnameOut = SURNAMES[randomIndexSurname];
 
-  var nameWizard =  nameOut + surnameOut;
+  var nameWizard = nameOut + surnameOut;
 
   names.push(nameWizard);
-};
-//генерация случайного цветс глаз
+}
+// генерация случайного цветс глаз
 var colors = [];
-for (var w = 0; w < coatColor.length; w++){
-  var  randomIndexColor= Math.floor(Math.random() * coatColor.length);
+for (var w = 0; w < coatColor.length; w++) {
+  var randomIndexColor = Math.floor(Math.random() * coatColor.length);
   var coatWizard = coatColor[randomIndexColor];
   colors.push(coatWizard);
-};
-//генерация случайного цвета глаз
+}
+// генерация случайного цвета глаз
 var eyes = [];
-for (var x = 0; x < coatColor.length; x++){
-  var  randomIndexEye= Math.floor(Math.random() * eyesColor.length);
+for (var x = 0; x < coatColor.length; x++) {
+  var randomIndexEye = Math.floor(Math.random() * eyesColor.length);
   var eyesWizard = eyesColor[randomIndexEye];
   eyes.push(eyesWizard);
-};
+}
 
 // массив параметров персонажа
 var wizards = [
@@ -68,8 +68,8 @@ var wizards = [
     eyesColor: eyes[3]
   }
 ];
-//создаем персонажей
-var renderWizard = function(wizard) {
+// создаем персонажей
+var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
@@ -77,7 +77,7 @@ var renderWizard = function(wizard) {
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
-}
+};
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
@@ -86,4 +86,3 @@ for (var i = 0; i < wizards.length; i++) {
 similarListElement.appendChild(fragment);
 
 SETUP_WINDOW.querySelector('.setup-similar').classList.remove('hidden');
-
